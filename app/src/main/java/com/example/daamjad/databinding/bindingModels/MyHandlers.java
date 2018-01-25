3cad86f66c7d311b6e622e4ecffe2ad160c977f9
daamjad <daamjad@creativechaos.co>
2018-01-25 13:05:02 +0500

    Added Handlers

---
+++ app/src/main/java/com/example/daamjad/databinding/bindingModels/MyHandlers.java
@@ -0,0 +1,14 @@
+package com.example.daamjad.databinding.bindingModels;
+
+import android.view.View;
+
+/**
+ * Created by daamjad on 1/25/2018.
+ */
+
+public class MyHandlers
+{
+    public void onClick(View view) {
+
+    }
+}
